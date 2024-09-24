a549c6561631c5f184a33d37b8097532150cfba8
Troy Underwood <troyizzle@gmail.com>
2024-09-24 15:53:07 -0400

    feat: add textarea component

---
+++ components/ui/textarea_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module UI
+  class TextareaComponent < BaseComponent
+    def self.default_class
+      'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"'
+    end
+
+    def class_names
+      super(self.class.default_class)
+    end
+
+    def call
+      tag.textarea class: class_names, **opts
+    end
+  end
+end
